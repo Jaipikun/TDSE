ab392bdb3d145b87142493eae7bd6a0ad8ad8e2c
Jaipikun <maska989@gmail.com>
2023-05-29 16:16:05 +0200

    Initialized project, wrote some guidelines

---
+++ main.py
@@ -0,0 +1,16 @@
+import method_1,method_2
+
+#Authors
+
+
+#maina nie ruszamy aż do finału :)
+
+
+
+def main():
+    return 0
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ method_2.py
@@ -0,0 +1,18 @@
+#importy
+
+#Author
+
+
+
+#metoda
+
+
+
+
+#main do testów własnych
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
